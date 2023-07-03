d0252ad2be29024294828101c2deb96b2323005f
WilliamLima300 <williamlima300@gmail.com>
2023-07-03 16:41:38 -0300

    update

--- src/Pages/Privado/index.jsx
+++ src/Pages/Privado/index.jsx
@@ -1,28 +1,75 @@
-import React ,{ useContext, useState }from 'react'
-import { UserContext } from '../../../context/UserProvider';
-import { StyleSheet, View } from 'react-native';
+import React ,{ useContext, useState,useEffect, useRef }from 'react'
+import { StyleSheet, View,Platform } from 'react-native';
 import NavigationBar from '../navBar';
 import { ContactContext } from '../../../context/ContacProvider';
 import Contatos from '../../Contatos/index';
 import ChatComponent from '../../Chat/index';
-import SendNotifications from '../../useNotifications';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import apiUser from '../../../apiUser';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 export default function ChatPrivado() {
-    const { selectedContact } = useContext(ContactContext);
-    const {Deslogar} = useContext(UserContext);
-    const [view, setView] = useState("lista")
-    
-    const views = {
-      lista:   <Contatos tipo="private"/>,
-      chat: <ChatComponent tipo="private" style={styles.container}/>,
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  const { selectedContact } = useContext(ContactContext);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    if(expoPushToken){
+      insertExpoToken();
     }
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  function insertExpoToken() {
+    let token = expoPushToken;
+    apiUser.post('/notifications/registerExpoToken', {token})
+    .then((response) => {
+        if(response.data.error){
+            alert(response.data.error);
+        }else{
+            console.log(response.data)
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+  }
+
+    
+  const views = {
+    lista:   <Contatos tipo="private"/>,
+    chat: <ChatComponent tipo="private" style={styles.container}/>,
+  }
 
   return (
     <View style={styles.container}>
       {selectedContact ? views.chat : views.lista}
       
       {!selectedContact ? <NavigationBar currentPage='Chat Privado'/> : null}
-      <SendNotifications style={{display:"none"}}/>
     </View>
 
   )
@@ -48,4 +95,37 @@ const styles = StyleSheet.create({
         marginTop:10,
     },
   });
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+  
+    if (Platform.OS === 'android') {
+      await Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    return token;
+  }
+  
 
