c0bb57a25b2433c5155279b7709cfa1c68c03e67
WilliamLima300 <williamlima300@gmail.com>
2023-07-07 11:34:49 -0300

    update

--- src/Chat/MessageSender/fileInput.jsx
+++ src/Chat/MessageSender/fileInput.jsx
@@ -1,42 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, TouchableOpacity, StyleSheet, Pressable, Text } from "react-native";
 import { Icon } from "@rneui/themed";
 import * as DocumentPicker from 'expo-document-picker';
 
 export default function FileInput(props) {
-    const pickDocument = async () => {
-      try {
-        let result = await DocumentPicker.getDocumentAsync({});
-        if (result.type === "success" && result) {
-          props.setFiles([...props.files, result]); // Adiciona o arquivo selecionado ao array de arquivos
-        }
-      } catch (error) {
-        console.log('Error picking document:', error);
+  const [showFiles, setShowFiles] = useState(false);
+
+  useEffect(() => {
+    if (props.files.length > 0) {
+      setShowFiles(true);
+    } else {
+      setShowFiles(false);
+    }
+  }, [props.files]);
+
+  const pickDocument = async () => {
+    try {
+      let result = await DocumentPicker.getDocumentAsync({});
+      if (result.type === "success" && result) {
+        props.setFiles([...props.files, result]); // Adiciona o arquivo selecionado ao array de arquivos
       }
-    };
-  
-    return (
-      <>
-        <TouchableOpacity style={styles.iconsStyle} onPress={pickDocument}>
-          <Icon name="attach-sharp" type="ionicon" size={25} color={"#9ac31c"} />
-        </TouchableOpacity>
-        <View style={styles.container}>
-          {props.files.length > 0 && (
-            <View style={styles.filesShow}>
-              {props.files.map((file, index) => (
-                <View key={index} style={{ flexDirection: 'row', alignItems: 'center' }}>
-                  <Text style={{ color: "#FFF" }}>Arquivo em anexo: {file.name}</Text>
-                  <TouchableOpacity style={{ margin: 10 }} onPress={() => props.setFiles(prevFiles => prevFiles.filter((_, i) => i !== index))}>
-                    <Icon name="close-sharp" type="ionicon" size={25} color={"#9ac31c"} />
-                  </TouchableOpacity>
-                </View>
-              ))}
-            </View>
-          )}
-        </View>
-      </>
-    );
-  }
+    } catch (error) {
+      console.log('Error picking document:', error);
+    }
+  };
+
+  console.log(props.files.length);
+
+  return (
+    <>
+      <TouchableOpacity style={styles.iconsStyle} onPress={pickDocument}>
+        <Icon name="attach-sharp" type="ionicon" size={25} color={"#9ac31c"} />
+      </TouchableOpacity>
+      <View style={styles.container}>
+        {showFiles && (
+          <View style={styles.filesShow}>
+            {props.files.map((file, index) => (
+              <View key={index} style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Text style={{ color: "#FFF" }}>Arquivo em anexo: {file.name}</Text>
+                <TouchableOpacity style={{ margin: 10 }} onPress={() => props.setFiles(prevFiles => prevFiles.filter((_, i) => i !== index))}>
+                  <Icon name="close-sharp" type="ionicon" size={25} color={"#9ac31c"} />
+                </TouchableOpacity>
+              </View>
+            ))}
+          </View>
+        )}
+      </View>
+    </>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -44,16 +56,6 @@ const styles = StyleSheet.create({
     width: "100%",
     position: "absolute",
     bottom: 0,
-  },
-  textInput: {
-    paddingHorizontal: 10,
-    height: 40,
-    width: "75%",
-    borderColor: "#142a4c",
-    borderTopWidth: 1,
-  },
-  messageList: {
-    flexGrow: 1,
   },
   iconsView: {
     width: "25%",
@@ -64,17 +66,17 @@ const styles = StyleSheet.create({
     borderColor: "#142a4c",
     borderTopWidth: 1,
     borderLeftWidth: 1,
-  },filesShow:{
+  },
+  filesShow: {
     position: 'absolute',
     bottom: 40,
     right: 0,
-    width: '200%',
+    width: '300%',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     backgroundColor: '#142a4c',
-    color:"#fff",
-    display: 'flex',
+    color: '#fff',
     justifyContent: 'center',
-    },
+  },
 });
