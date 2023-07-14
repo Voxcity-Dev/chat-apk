5fc47ef789a89dd1e4d60e3b1d174f5e5c025ce2
vox-dev <voxcitydev@gmail.com>
2023-07-14 13:59:38 -0300

    will pairing

--- src/Chat/index.jsx
+++ src/Chat/index.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useContext,useEffect} from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { View } from 'react-native';
 import MessageScreen from './MessageScreen/messageScreen';
 import MessageSender from './MessageSender/messageSender';
@@ -7,28 +7,31 @@ import { GroupContext } from '../../context/GroupProvider';
 import { AttendanceContext } from '../../context/AttendanceProvider';
 
 export default function ChatComponent(props) {
-  const { selectedContact,contacts } = useContext(ContactContext);
-  const { selectedGroup,groups } = useContext(GroupContext);
-  const { selectedAtendimento,attendances } = useContext(AttendanceContext);
+  const { selectedContact, contacts } = useContext(ContactContext);
+  const { selectedGroup, groups } = useContext(GroupContext);
+  const { selectedAtendimento, attendances } = useContext(AttendanceContext);
   const [contato, setContato] = useState({});
 
   useEffect(() => {
     let newContact = {};
     if (props.tipo === 'private') {
-      newContact = {...selectedContact}
+      newContact = { ...selectedContact }
     } else if (props.tipo === 'group') {
-      newContact = {...selectedGroup}
+      newContact = { ...selectedGroup }
     } else if (props.tipo === 'att') {
-      newContact = {...selectedAtendimento}
+      newContact = { ...selectedAtendimento }
     }
-    setContato(newContact);
-  }, [contacts,groups,attendances,selectedContact,selectedGroup,selectedAtendimento]);
+
+    if (contato._id && newContact._id === contato._id) {
+      setContato(newContact);
+    }
+  }, [contacts, groups, attendances, selectedContact, selectedGroup, selectedAtendimento]);
 
   return (
-    <View style={{ flex: 1,width:"100%",height:"100%" }}>
+    <View style={{ flex: 1, width: "100%", height: "100%" }}>
 
       <MessageScreen tipo={props.tipo} contato={contato} />
-      
+
       <MessageSender tipo={props.tipo} contato={contato} />
 
     </View>
