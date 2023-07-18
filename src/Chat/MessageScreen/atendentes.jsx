c2f7cb5915c41712191aa6bc5cd959c96149489f
vox-dev <voxcitydev@gmail.com>
2023-07-18 16:07:52 -0300

    update

--- src/Chat/MessageScreen/atendentes.jsx
+++ src/Chat/MessageScreen/atendentes.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { Icon } from '@rneui/themed';
 export default function Atendentes(props) {
-    const { attendances, searchList, transferContactToAtendente,styles } = props;
+    const { attendances, searchList, transferContactToAtendente } = props;
     return (
         <ScrollView style={{ width: "100%", height: "30%" }}>
             {
@@ -53,3 +53,49 @@ export default function Atendentes(props) {
         </ScrollView>
   )
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: '#FFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignContent: "center",
+        width: "100%",
+    },
+    text: {
+        color: "#142a4c",
+        fontSize: 16,
+        fontWeight: "bold",
+        marginTop: 10,
+        marginLeft: 10,
+        width: "50%",
+        borderBottomWidth: 1,
+        borderColor: "#9ac31c",
+        textAlign: "center"
+    },
+    blocoContato: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "80%",
+        padding: 10,
+        backgroundColor: "#f1f1f1",
+        borderWidth: 1,
+        borderColor: "#142a4c",
+        marginLeft: 50,
+        marginTop: 10,
+        marginBottom: 10,
+        marginRight: 50,
+    },
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        marginRight: 10,
+    },
+
+});
