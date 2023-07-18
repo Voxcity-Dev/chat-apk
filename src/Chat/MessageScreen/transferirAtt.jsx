de5ff38fb55620267f84a30de6c83fa894a1271e
vox-dev <voxcitydev@gmail.com>
2023-07-18 16:27:18 -0300

    update

--- src/Chat/MessageScreen/transferirAtt.jsx
+++ src/Chat/MessageScreen/transferirAtt.jsx
@@ -89,7 +89,7 @@ export default function Transferir() {
                 />
             </View>
             {
-                search && <>
+               <>
                     {views[setView]}
                 </>
 
