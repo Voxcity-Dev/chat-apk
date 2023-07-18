730ced9c01f19f833c2b6e4317a21819cb857f9c
vox-dev <voxcitydev@gmail.com>
2023-07-18 17:21:18 -0300

    update

--- src/Atendimento/types/meusAtendimento.jsx
+++ src/Atendimento/types/meusAtendimento.jsx
@@ -1,12 +1,12 @@
-import React,{useContext,useState} from 'react'
-import { View,StyleSheet,Text, TouchableOpacity,Image } from 'react-native';
+import React, { useContext, useState } from 'react'
+import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
 import { AttendanceContext } from '../../../context/AttendanceProvider';
 //import { Icon } from '@rneui/themed';
 //import NovoAtendimento from './novoAtendimento';
 
-export default  function MeusAtendimentos (props) {
+export default function MeusAtendimentos(props) {
   const { setSelectedAtendimento } = useContext(AttendanceContext);
- // const [novoAtendimento, setNovoAtendimento] = useState(false);
+  // const [novoAtendimento, setNovoAtendimento] = useState(false);
 
   function countAndSlice(message) {
     if (!message) return ""
@@ -18,12 +18,12 @@ export default  function MeusAtendimentos (props) {
     return message
   }
 
-  function nameInsert(att){
-    if(att.pushname){
+  function nameInsert(att) {
+    if (att.pushname) {
       return att.pushname
-    }else if(att.nome){
+    } else if (att.nome) {
       return att.nome
-    }else{
+    } else {
       return att.telefone
     }
   }
@@ -33,15 +33,15 @@ export default  function MeusAtendimentos (props) {
     return time;
   }
 
-  function handleSelectedAtendimento(att){
+  function handleSelectedAtendimento(att) {
     setSelectedAtendimento(att);
   }
 
 
   return (
-   <View style={styles.container}>
-    <Text style={styles.text}>Meus Atendimentos</Text>
-    {/* {
+    <View style={styles.container}>
+      <Text style={styles.text}>Meus Atendimentos</Text>
+      {/* {
       novoAtendimento ? <NovoAtendimento setNovoAtendimento={setNovoAtendimento}/> : 
       <View >
         <TouchableOpacity style={styles.buttonBox} onPress={()=>setNovoAtendimento(true)}>
@@ -49,98 +49,98 @@ export default  function MeusAtendimentos (props) {
           <Text style={styles.buttonText}>Novo Atendimento</Text>
         </TouchableOpacity> */}
 
-        {
-          props.atendimentos.map((att) => {
-            let lastMsgTime
-            let formatedDate
-            let formatedTime
-            if (att.lastMessage) {
-              lastMsgTime = new Date(att.lastMessage.createdAt);
-              formatedDate = timeHasZero(lastMsgTime.getDate()) + "/" + (timeHasZero(lastMsgTime.getMonth() + 1)) + "/" + timeHasZero(lastMsgTime.getFullYear());
-              formatedTime = timeHasZero(lastMsgTime.getHours()) + ":" + timeHasZero(lastMsgTime.getMinutes());
-            }
-
-            return (
-              <TouchableOpacity style={styles.contactBox} key={att._id} onPress={ () => handleSelectedAtendimento(att) }>
-                {
-                  att.foto ? <Image source={{ uri: att.foto }} style={styles.image}/> : <Image source={require('../../../assets/avatar2.png')} style={styles.image}/> 
-                }
-                <View style={styles.contactInfo}>
-                  <Text style={styles.buttonText}>{nameInsert(att)}</Text>
-                  <Text style={{color:"#142a4c",fontSize:12}}>{countAndSlice(att.lastMessage?.message)}</Text>
-                  <Text style={{color:"#142a4c",fontSize:12}}>{formatedDate} {formatedTime}</Text>
-                </View>
-              </TouchableOpacity>
-            )
+      {
+        props.atendimentos.map((att) => {
+          let lastMsgTime
+          let formatedDate
+          let formatedTime
+          if (att.lastMessage) {
+            lastMsgTime = new Date(att.lastMessage.createdAt);
+            formatedDate = timeHasZero(lastMsgTime.getDate()) + "/" + (timeHasZero(lastMsgTime.getMonth() + 1)) + "/" + timeHasZero(lastMsgTime.getFullYear());
+            formatedTime = timeHasZero(lastMsgTime.getHours()) + ":" + timeHasZero(lastMsgTime.getMinutes());
           }
+
+          return (
+            <TouchableOpacity style={styles.contactBox} key={att._id} onPress={() => handleSelectedAtendimento(att)}>
+              {
+                att.foto ? <Image source={{ uri: att.foto }} style={styles.image} /> : <Image source={require('../../../assets/avatar2.png')} style={styles.image} />
+              }
+              <View style={styles.contactInfo}>
+                <Text style={styles.buttonText}>{nameInsert(att)}</Text>
+                <Text style={{ color: "#142a4c", fontSize: 12 }}>{countAndSlice(att.lastMessage?.message)}</Text>
+                <Text style={{ color: "#142a4c", fontSize: 12 }}>{formatedDate} {formatedTime}</Text>
+              </View>
+            </TouchableOpacity>
           )
         }
+        )
+      }
 
-        {
-          props.atendimentos.length === 0 ? <Text style={{color:"#142a4c",fontSize:12,textAlign:"center"}}>Nenhum atendimento encontrado</Text> : null
-        }
-      </View>
+      {
+        props.atendimentos.length === 0 ? <Text style={{ color: "#142a4c", fontSize: 12, textAlign: "center" }}>Nenhum atendimento encontrado</Text> : null
+      }
+    </View>
     //}
-        
+
     //</View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    width:"100%",
+    width: "100%",
     flex: 1,
     backgroundColor: '#FFF',
     color: '#111',
-    alignItems:"center",
+    alignItems: "center",
   },
-  buttonBox:{
-    width:"100%",
-    height:50,
-    display:"flex",
-    alignItems:"center",
-    justifyContent:"center",
-    flexDirection:"row",
-    padding:10,
+  buttonBox: {
+    width: "100%",
+    height: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    padding: 10,
   },
-  buttonText:{
-    color:"#142a4c",
-    fontSize:16,
-    fontWeight:"bold",
-    marginTop:10,
+  buttonText: {
+    color: "#142a4c",
+    fontSize: 16,
+    fontWeight: "bold",
+    marginTop: 10,
   },
   image: {
     width: 50,
     height: 50,
     borderRadius: 100,
-    marginLeft:10,
-    marginTop:10,
+    marginLeft: 10,
+    marginTop: 10,
   },
   contactBox: {
-    width:"100%",
-    height:80,
-    display:"flex",
-    alignItems:"flex-start",
-    justifyContent:"flex-start",
-    flexDirection:"row",
-    color:"#142a4c",
-    padding:10,
-  },    
-  contactInfo:{
-    width:"100%",
-    height:100,
-    marginLeft:20,
+    width: "100%",
+    height: 80,
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    color: "#142a4c",
+    padding: 10,
   },
-  text:{
-    color:"#142a4c",
-    fontSize:16,
-    fontWeight:"bold",
-    marginTop:10,
-    marginLeft:10,
-    width:"50%",
-    borderBottomWidth:1,
-    borderColor:"#9ac31c",
-    textAlign:"center"
+  contactInfo: {
+    width: "100%",
+    height: 100,
+    marginLeft: 20,
+  },
+  text: {
+    color: "#142a4c",
+    fontSize: 16,
+    fontWeight: "bold",
+    marginTop: 10,
+    marginLeft: 10,
+    width: "50%",
+    borderBottomWidth: 1,
+    borderColor: "#9ac31c",
+    textAlign: "center"
   },
 
 });
